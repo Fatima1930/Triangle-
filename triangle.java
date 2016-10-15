3d04d966341f47f3a6f3bd29b09afac6f23a7dce
oussoukine <oussoukineracha7@gmail.com>
2016-10-15 20:13:16 +0200

    Update triangle.java

---
+++ triangle.java
@@ -0,0 +1,11 @@
+public static void Triangle(int h){
+String etoile=" ";int i=j=0;int espace;
+for(i=0;i<h;i++){
+espace=h-i;
+for{j=0;j<espace;j++){
+System.out.print(" ");}
+etoile=etoile+"* ";
+System.out.println(etoile);}
+public static void main(String[] args){
+Triangle (8);
+}}
