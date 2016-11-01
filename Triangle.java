fc1f494a787d653e29332c059df22f0a6311b5e0
Fatima1930 <labonne1930@hotmail.fr>
2016-11-01 19:13:23 +0100

    Update Triangle.java
    j ai ajouter deux commentaire comme //**essai**//

--- Triangle.java
+++ Triangle.java
@@ -2,12 +2,14 @@ public class Triangle {
 
 	public static void main(String[] args) {
 		String etoile=" ";
+		// i reprsente le nombre de ligne//
 		for (int i=0;i<8;i++){
 			int espace=8-i;
+			//j nombre des etoiles//
 			for (int j=0;j<espace;j++){
 				System.out.print(" ");
 			}
-			etoile=etoile+"* ";
+			etoile=etoile+" ";
 			System.out.println(etoile);
 		}
 
